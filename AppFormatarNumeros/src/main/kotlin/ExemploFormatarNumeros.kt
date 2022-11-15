69cd06aa118b27ef40ac4e63f3bc1a1e2fd40c7c
Joolmaa <102316347+Joolmaa@users.noreply.github.com>
2022-11-15 00:28:42 -0300

    Add files via upload

---
+++ AppFormatarNumeros/src/main/kotlin/ExemploFormatarNumeros.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val numero = 10
+
+    val saidaFormatada = String.format("%012d",numero)
+
+    val soma = numero + numero
+
+    print("Número: $numero\n")
+    print("Padão de formatação: $saidaFormatada\n")
+    print("Soma: $soma")
+
+
+
+
+
+
+}
